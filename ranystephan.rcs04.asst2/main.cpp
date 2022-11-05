56fb98de2198d5423ecd982ef7537e7423126e8c
ranystephan <63688460+ranystephan@users.noreply.github.com>
2022-11-05 16:20:40 +0200

    Initial Commit

---
+++ ranystephan.rcs04.asst2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ranystephan.rcs04.asst2
+//
+//  Created by Rany STEPHAN on 11/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
